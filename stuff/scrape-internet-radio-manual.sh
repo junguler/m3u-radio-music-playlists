2b0e79a21130345dd85d23bc16f6fec9f761a3f8
hossein s. borhani <junguler@yahoo.com>
2022-11-29 00:38:15 +0330

    Update scrape-internet-radio-manual.sh

--- stuff/scrape-internet-radio-manual.sh
+++ stuff/scrape-internet-radio-manual.sh
@@ -11,6 +11,7 @@ for i in "" page{2..16} ; do for j in $(cat links2.txt) ; do curl -s https://www
 
 # a few links have more than 16 pages, the longest page is pop with 50 pages, so if you need all of them you have to run another for loop for them
 # these streams need to be scraped for 34 pages or less furthur totaling of 50 pages = "Glam Rock" "Rock" "Classic" "Rock" "Pop"
+# for i in page{17..50} ; do for j in "classic%20rock" "glam%20rock" "pop" "rock" ; do curl -s https://www.internet-radio.com/stations/$j/$i.html | htmlq --attribute href a | grep '.m3u' | cut -b 37- | awk -F '\\listen' '{print $1""}' | awk -F '\\.m3u' '{print $1""}' | awk -F '\\&t=' '{print $1""}' | awk '!seen[$0]++' | sed '/^$/d' | awk 'length>10' >> $j.txt ; echo "$j - $i scraped" ; done ; done
 
 # convert links to m3u stream files
 for i in $(cat links2.txt) ; do sed "s/^/#EXTINF:-1\n/" $i.txt | sed '1s/^/#EXTM3U\n/' > $i.m3u ; done
