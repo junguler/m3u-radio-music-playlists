eb59717a039ec567f414ad727c5f8ae7a462d435
U-DESKTOP-3VIS1T9\junguler <junguler@yahoo.com>
2023-04-15 21:29:39 +0330

    update scrape-streema-manual script
    this remove empty stream titles

--- stuff/scrape-streema-manual.sh
+++ stuff/scrape-streema-manual.sh
@@ -16,7 +16,7 @@ for i in "" \?page={2..10} ; do for j in $(cat countries.txt) ; do curl -s https
 for i in A-*.txt ; do for j in $(cat $i) ; do curl -s http://streema.com/radios/play/$j > mep1 ; cat mep1 | htmlq -t h3 | awk NF | sed -e 's/[ \t]*//' | awk '{print "#EXTINF:-1 , "$0}' >> A$i ; cat mep1 | grep "data-src=" | awk -F "'" '{print $2}' | sed 's/https:\/\/stream.streema.com\/?url=//g' | sed 's/\;//g' | grep -v "<div id" | sed '/^$/d' >> A$i ; echo -e "$i - $j" ; done ; done
 
 # remove possible duplicates in streams
-for i in AA-*.txt ; do cat $i | awk '!seen[$0]++' > A$i ; echo -e $i ; done
+for i in AA-*.txt ; do cat $i | awk '!seen[$0]++' | grep -B1 "http" > A$i ; echo -e $i ; done
 
 # convert links to m3u streams
 for i in AAA-*.txt ; do sed '1s/^/#EXTM3U\n/' $i > $i.m3u ; done
