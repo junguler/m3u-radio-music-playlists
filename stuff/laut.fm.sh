258a3cd8e8929a60d252e23707d033439093f717
U-DESKTOP-3VIS1T9\junguler <junguler@yahoo.com>
2023-06-12 15:52:40 +0330

    add laut.fm script

---
+++ stuff/laut.fm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# find genres 
+curl -s https://laut.fm/fm-api/genres | jq -r '.[].name' | sed 's/ /%20/g' | sort > genres.txt
+
+# find every page title within each genres
+for i in $(cat genres.txt) ; do curl -s https://laut.fm/fm-api/stations/genre/$i?offset=0&limit=10000 | jq -r '.items[].name' >> A-$i.txt ; echo -e $i ; done
+
+# convert page titles to streams, no need to put any further load on the website
+for i in A-*.txt ; do for j in $(cat $i) ; do echo "#EXTINF:-1 , $(echo $j | sed -e 's/-/ /g' -e 's/_/ /g')" >> A$i ; echo "http://stream.laut.fm/$j" >> A$i ; echo -e $i ; done ; done
+
+# convert links to m3u streams
+for i in AA-*.txt ; do sed '1s/^/#EXTM3U\n/' $i > $i.m3u ; done
+
+# remove AA- and double extensions in streams
+for i in *.m3u ; do mv "$i" "`echo $i | sed -e 's/AA-//' -e 's/.txt//'`" ; done
+
+# replace %20 in file names with _
+for i in *.m3u ; do mv -- "$i" "$(printf '%s\n' "$i" | sed 's/%20/_/')" ; done
