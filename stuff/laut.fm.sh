b297ad4e907d3b6dce36e81c301f21b13e076ce4
U-DESKTOP-3VIS1T9\junguler <junguler@yahoo.com>
2023-06-13 04:30:46 +0330

    update laut.fm script

--- stuff/laut.fm.sh
+++ stuff/laut.fm.sh
@@ -6,8 +6,11 @@ curl -s https://laut.fm/fm-api/genres | jq -r '.[].name' | sed 's/ /%20/g' | sor
 # find every page title within each genres
 for i in $(cat genres.txt) ; do curl -s https://laut.fm/fm-api/stations/genre/$i?offset=0&limit=10000 | jq -r '.items[].name' >> A-$i.txt ; echo -e $i ; done
 
+# make sure text files are linux compatible
+for i in A-*.txt ; do sed -i 's/\r$//' $i ; done
+
 # convert page titles to streams, no need to put any further load on the website
-for i in A-*.txt ; do for j in $(cat $i) ; do echo "#EXTINF:-1 , $(echo $j | sed -e 's/-/ /g' -e 's/_/ /g')" >> A$i ; echo "http://stream.laut.fm/$j" >> A$i ; echo -e $i ; done ; done
+for i in A-*.txt ; do for j in $(cat $i) ; do echo "#EXTINF:-1 , $(echo $j | sed -e 's/-/ /g' -e 's/_/ /g')\nhttp://$j.stream.laut.fm/$j" >> A$i ; done ; done
 
 # convert links to m3u streams
 for i in AA-*.txt ; do sed '1s/^/#EXTM3U\n/' $i > $i.m3u ; done
