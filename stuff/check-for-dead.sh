12869f52227d0f5472ead9ef010e1e6e390b5d28
U-DESKTOP-3VIS1T9\junguler <junguler@yahoo.com>
2024-02-07 15:38:52 +0330

    clean-up check-for-dead script

--- stuff/check-for-dead.sh
+++ stuff/check-for-dead.sh
@@ -7,7 +7,7 @@ for i in *.m3u ; do sed -i 's/ /_/g' $i ; done
 for i in *.m3u ; do for j in $(cat $i) ; do wget -S --spider -q -t 1 -T 1 --max-redirect 0 $j 2>&1 | grep "HTTP/" | awk '{print $2}' | (cat ; echo $j ;) | paste -s -d " " >> A-$i ; echo -e "$i - $j" ; done ; done
 
 # find links that have 200 response code and remove others
-for i in A-*.m3u ; do cat $i | grep -B1 "200 " | sed -e 's/200 //g' -e 's/_/ /g' | awk 'length>3' | grep -A1 "#" | sed 's/ , /,/g' > A$i ; done
+for i in A-*.m3u ; do cat $i | grep -B1 "200 " | sed 's/200 //g' | awk 'length>3' | grep -A1 "#" > A$i ; done
 
 # remove extra fluff from file names
 #for i in AA-*.m3u ; do mv $i $(echo $i | sed 's/AA-//') ; done
