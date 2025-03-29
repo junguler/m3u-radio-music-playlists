38b193f5fc87da318cec5840e0bc0345058314ad
U-DESKTOP-3VIS1T9\junguler <junguler@yahoo.com>
2025-03-29 22:39:43 +0330

    update online_radio_box folder

--- stuff/online_radio_box.sh
+++ stuff/online_radio_box.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 # scrape the links from internet radio
-#lynx --dump --listonly --nonumbers https://onlineradiobox.com/genres/ | grep "https://onlineradiobox.com/genre" | grep -v "genres" | sort | uniq > links.txt
 curl -s https://onlineradiobox.com/genres/ | htmlq -a href a | grep "genre" | grep -v "genres" | sort | uniq | awk -F "/" '{print $3}' > genres.txt
 
-# strip unnessery part of links (we'll add them later in the for loop) 
-#cat links.txt | rev | cut -c2- | rev | cut -c34- > pages.txt
-
 # scrape links of the streams
-#for i in $(cat pages.txt) ; do for j in "" \?p={1..200} ; do curl https://onlineradiobox.com/genre/$i/$j | grep -oP 'stream="\K[^"]+' | grep -v "playerservices\|.m3u\|onlineradiobox" | sed 's/\;//g' | awk '!seen[$0]++' | sed '/^$/d' | awk 'length>10' >> $i.txt ; echo "$i - $j scraped" ; done ; done
-for i in $(cat genres.txt) ; do curl -s https://onlineradiobox.com/genre/$i/ | htmlq .stations-list button '.station_play, .b-play' | grep -oP 'radioname="\K[^"]+|stream="\K[^"]+' | sed 's/\;//g' >> A-$i.txt ; echo -e "$i" ; done 
-for i in $(cat genres.txt) ; do for j in \?p={1..200} ; do curl -s https://onlineradiobox.com/genre/$i/$j | htmlq .stations-list button '.station_play, .b-play' | grep -oP 'radioname="\K[^"]+|stream="\K[^"]+' | sed 's/\;//g' >> A-$i.txt ; echo -e "$i - $j" ; done ; done
+for i in $(cat genres.txt) ; do curl --socks5 127.0.0.1:10808 -s https://onlineradiobox.com/genre/$i/ | htmlq .stations-list button '.station_play, .b-play' | sed -r 's/.*radioname="([^"]*)".*stream="([^"]*)".*/#EXTINF:-1,\1\n\2/' | sed 's/\;//g' >> A-$i.txt ; echo -e "$i" ; done
+for i in $(cat genres.txt) ; do for j in \?p={1..200} ; do curl -s https://onlineradiobox.com/genre/$i/$j | htmlq .stations-list button '.station_play, .b-play' | sed -r 's/.*radioname="([^"]*)".*stream="([^"]*)".*/#EXTINF:-1,\1\n\2/' | sed 's/\;//g' >> A-$i.txt ; echo -e "$i - $j" ; done ; done
 
-# convert temp files to proper format
-for i in A-*.txt ; do cat $i | awk '!seen[$0]++' | awk '{print "#EXTINF:-1,"$0}' | sed 's/\;//g' | sed 's/#EXTINF:-1 , http/http/g' > A$i ; done
+# remove streams that didn't have links
+for i in A-*.txt ; do cat $i | awk '!seen[$0]++' | grep -B1 "http" | grep -A1 "EXTINF" | awk 'length>4' > A$i ; echo -e $i ; done
 
 # convert files to m3u extension
 for i in AA-*.txt ; do sed '1s/^/#EXTM3U\n/' $i > $i.m3u ; done
